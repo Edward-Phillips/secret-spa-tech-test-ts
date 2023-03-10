48f922259cf7574f262a857fe205aa7b89ca207a
edwardPhillips <edd0ddi0@gmail.com>
2023-03-10 07:59:41 +0000

    feat: 1 = format DayItem text to `MMM Do YYYY` format

--- src/components/DayItem.tsx
+++ src/components/DayItem.tsx
@@ -1,9 +1,11 @@
+import moment from "moment";
+
 interface Props {
   day: string;
 }
 
 const DayItem = ({ day }: Props) => {
-  return <button className="dayItem">{day}</button>;
+  return <button className="dayItem">{moment(day).format("MMM Do YYYY")}</button>;
 };
 
 export default DayItem;
